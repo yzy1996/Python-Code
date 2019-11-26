161a46379639fff1b50e343feb5953e66e0ac0f8
yzy1996 <yang3781590@163.com>
2019-11-26 23:38:08 +0800

    Update DFS.py

--- Algorithm/Search-Algorithm/DFS.py
+++ Algorithm/Search-Algorithm/DFS.py
@@ -1,2 +1,27 @@
 # 深度优先算法
 
+
+def iter_dfs(G, s):  # G是整个图， s是起点
+    S, Q = set(), []  # S是
+    Q.append(s)
+    while Q:
+        u = Q.pop()
+        if u in S: continue
+        S.add(u)
+        Q.extend(G[u])
+        yield u
+
+
+if __name__ == "__main__":
+    a, b, c, d, e, f, g, h, i = range(9)
+    G = [
+        {b, c, d, e, f},  # a
+        {c, e},  # b
+        {d},  # c
+        {e},  # d
+        {f},  # e
+        {c, g, h},  # f
+        {f, h},  # g
+        {f, g}  # h
+    ]
+    print(list(iter_dfs(G, a)))  # [0, 5, 7, 6, 2, 3, 4, 1]
