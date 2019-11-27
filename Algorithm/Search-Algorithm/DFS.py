fcb45fca62567efb92d521812f562400e80f715e
yzy1996 <yang3781590@163.com>
2019-11-27 09:27:59 +0800

    Update DFS.py

--- Algorithm/Search-Algorithm/DFS.py
+++ Algorithm/Search-Algorithm/DFS.py
@@ -1,14 +1,16 @@
-# 深度优先算法
-
+'''深度优先算法
+算法逻辑：
+从初始点开始，向子节点搜索，
+'''
 
 def iter_dfs(G, s):  # G是整个图， s是起点
-    S, Q = set(), []  # S是
-    Q.append(s)
-    while Q:
-        u = Q.pop()
+    S, Q = set(), []  # S是存放具体的访问路径
+    Q.append(s)  # Q是用来存放需要进行遍历的数据
+    while Q:  # 只要不是空
+        u = Q.pop()  # 删除并返回末尾元素
         if u in S: continue
         S.add(u)
-        Q.extend(G[u])
+        Q.extend(G[u])  # 在末尾追加
         yield u
 
 
