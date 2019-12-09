c2cfa6fdb8922fb6599358f77628b97f18637b2d
crazyang <yang3781590@163.com>
2019-12-09 20:27:26 +0800

    Update DFS.py

--- Algorithm/Search-Algorithm/DFS.py
+++ Algorithm/Search-Algorithm/DFS.py
@@ -3,12 +3,14 @@
 从初始点开始，向子节点搜索，
 '''
 
+
 def iter_dfs(G, s):  # G是整个图， s是起点
     S, Q = set(), []  # S是存放具体的访问路径
     Q.append(s)  # Q是用来存放需要进行遍历的数据
     while Q:  # 只要不是空
         u = Q.pop()  # 删除并返回末尾元素
-        if u in S: continue
+        if u in S:
+            continue
         S.add(u)
         Q.extend(G[u])  # 在末尾追加
         yield u
