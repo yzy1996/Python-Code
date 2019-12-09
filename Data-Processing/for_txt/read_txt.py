8c193034a75a0778b92246cab7d1cee7690f6a84
crazyang <yang3781590@163.com>
2019-12-09 20:26:03 +0800

    Update read_txt.py

--- Data-Processing/for_txt/read_txt.py
+++ Data-Processing/for_txt/read_txt.py
@@ -1,6 +1,4 @@
-# 使用生成器表达式会非常高效，readline也不用了
-
-with open('xxx.txt') as f:
-    lines = (line.strip() for line in f)
+with open('example.txt') as f:
+    lines = (line.strip() for line in f)  # 得到一个迭代器
     for line in lines:
         print(line)
